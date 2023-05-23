55accb6beb5b3cd2fe5c9c8e0923456c56571b7e
Alh-19 <ana4599@gmail.com>
2023-05-23 09:43:56 +0200

    branch ana

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { Link } from 'react-router-dom';
-import  Navbar from '../../components/Navbar/Navbar'
-import logo from '../../assets/logosmall.png'
+import logo from '../../assets/logosmall.png';
+import Navbar from '../Navbar/Navbar';
+import cartIcon from '../../assets/image 5.png';
+import user from '../../assets/image 6.png';
+
 function Header() {
+
 
   return (
     <div className='container-header'>
-        <div className='container-navbar'>
-         
-           <Navbar />
 
+        <div className='container-navbar'>
+          <Navbar />
         </div>
+
         <div className='logo'>
             <Link to={`/`}><img src={logo}></img> </Link>
+        </div>
+
+        <div className='user'>
+          <a href=""> <img src={user} alt="user" /></a>
+        </div>
+
+        <div className='cart'>
+          <a href=""> <img src={cartIcon} alt="cart" /></a>
         </div>
 
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-
+import './Navbar.css'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
 
 const [ menu , setMenu ]= useState(false);
+
 const toggleMenu = () => {
     setMenu( !menu )
 }
+
   return (
-    <div>
-        <div className={`container-navbar ${menu ? 'isActive' : ''}`}>
-            <div className= 'close-menu'>
-            <FontAwesomeIcon icon={faBars} onClick={ toggleMenu }/>
-            </div>
-            <div className='menu'>
-                <ul>
-                    <li><a href="#">Women</a></li>
-                    <li><Link to={`/men`} />Men</li>
-                    <li><a href="#">New Arrivals</a></li>
-                    <li><a href="#">Sale</a></li>
-                    <li><a href="#">About Us</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <header className="Cabecera">
+        
+        <button 
+        onClick={ toggleMenu }
+        className="Cabecera-button">
+        <FontAwesomeIcon icon={faBars} size="2xl" className='icon-menu' />
+        </button>
+
+        <nav className={ `Cabecera-nav ${ menu ? 'isActive' : '' }` }>
+            <ul className="Cabecera-ul">
+                <li className="Cabecera-li"><a href="#" className="Cabecera-a">Women</a></li>
+                <li className="Cabecera-li"><a href="#" className="Cabecera-a"><Link to={`/men`} />Men</a></li>
+                <li className="Cabecera-li"><a href="#" className="Cabecera-a">New Arrivals</a></li>
+                <li className="Cabecera-li"><a href="#" className="Cabecera-a">Sale</a></li>
+                <li className="Cabecera-li"><a href="#" className="Cabecera-a">About us</a></li>
+            </ul>
+        </nav>
+
+    </header>
   );
 };
 
