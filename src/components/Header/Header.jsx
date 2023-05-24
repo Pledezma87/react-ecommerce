27387d251e899960a4af58f434e156a69f0bf286
Alh-19 <ana4599@gmail.com>
2023-05-24 09:47:54 +0200

    css header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,35 +5,37 @@ import logo from '../../assets/logosmall.png';
 import cartIcon from '../../assets/image 5.png';
 import user from '../../assets/image 6.png';
 import lupa from '../../assets/search.png';
+import './header.css';
+
 function Header() {
 
   return (
-    <div className='container-header'>
+      <div className='container-header'>
 
         <div className='container-navbar'>
-           <Navbar />
+          <Navbar />
         </div>
 
         <div className='logo'>
-            <Link to={`/`}><img src={logo}></img> </Link>
+          <Link to={`/`}><img src={logo} className='imagelogo'></img> </Link>
         </div>
+
         <div className='container-right'>
+          <div className='searchbar'>
+            <img src={lupa} className='lupa'></img>
+            <input type= "text" className="searchinput" placeholder="Search..."></input>
+          </div>
 
-        <div className='searchbar'>
-            <img src={lupa}></img>
-            <input type= "text" className = "searchinput"></input>
+          <div className='user'>
+            <a href=""> <img src={user} alt="user" /></a>
+          </div>
+
+          <div className='cart'>
+            <a href=""> <img src={cartIcon} alt="cart" /></a>
+          </div>
         </div>
 
-        <div className='user'>
-          <a href=""> <img src={user} alt="user" /></a>
-        </div>
-
-        <div className='cart'>
-          <a href=""> <img src={cartIcon} alt="cart" /></a>
-        </div>
-
-        </div>
-    </div>
+      </div>
   )
 }
 
