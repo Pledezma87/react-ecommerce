8adf8dce6aa1fc5d757e51f137fa90369e653e6e
Rocio <red200921@gmail.com>
2023-05-24 12:27:40 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
 
+import React from 'react'
+import'./footer.css'
 function Footer() {
   return (
 
@@ -7,10 +8,27 @@ function Footer() {
 
     <div className='titles'>
         <div className='ayuda'>
-            <a href=''>Ayuda</a>
+           <a href=''>ayuda</a>
+              <ul>
+              <li><span>Preguntas frecuentes</span></li>
+              <li><span>Estado de mi pedido</span></li>
+              <li><span>Cómo devolver</span></li>
+              <li><span>Tramitar devolución</span></li>
+              <li><span>Envíos</span></li>
+              <li><span>Tarjeta Regalo</span></li>
+            </ul>
         </div>
+
         <div className='empresa'>
             <a href=''>Empresa</a>
+            <ul>
+              <li><span>Quines somos</span></li>
+              <li><span>Localizador de tiendas</span></li>
+              <li><span>Cómo devolver</span></li>
+               <li><span>Franquicias</span></li>
+              <li><span>Envíos</span></li>
+               <li><span>Trabaja con nosotros</span></li>
+            </ul>
         </div>    
         <div className='Follow'>
             <a href=''>Follow us!</a>
