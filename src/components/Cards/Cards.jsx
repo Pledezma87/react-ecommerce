8b1e67d6dc88e7b63599dfb242ee171d700a481e
Pablo Cesar Ledezma <pablo.ledezma.br@gmail.com>
2023-05-24 09:47:52 +0200

    css cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import './Cards.css'; 
 
 const Product = ({ product, addToCart }) => {
   const { name, img, sizes, price } = product;
-  console.log(product)
-//   const imagePath ='./src/assets/';
 
   return (
-    <div className="cards">
+    <div className="cards"> 
       <img src={img} alt={name} />
       <h2>{name}</h2>
       {/* <p>Tallas: {sizes}</p> */}
-      <p>Precio: ${price}</p>
+      <p>${price}</p>
       <button onClick={() => addToCart(product)}>Añadir al carrito</button>
     </div>
   );
@@ -20,7 +19,7 @@ const Cards = ({ addToCart }) => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    // Simular una petición a la API para obtener los datos
+   
     fetch('http://localhost:3000/stock')
       .then((response) => response.json())
       .then((data) => {
@@ -29,16 +28,16 @@ const Cards = ({ addToCart }) => {
           name: item.name,
           price: item.price,
           img: item.img,
-        //   sizes: item.sizes,
+          //   sizes: item.sizes,
         }));
-        console.log(productsData)
+        console.log(productsData);
         setProducts(productsData);
       })
       .catch((error) => console.log(error));
   }, []);
 
   return (
-    <div className="card-container">
+    <div className="card-container"> 
       {products.map((product) => (
         <Product
           key={product.id}
